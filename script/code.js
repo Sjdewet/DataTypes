24b1bf84df0fd5b87c438914f05493d83ecf428b
Sjdewet <summerdewet@gmail.com>
2023-05-17 10:08:39 +0200

    update

---
+++ script/code.js
@@ -0,0 +1,30 @@
+//String
+let name = "Summer";
+console.log(typeof name);
+
+
+//number
+// let name = 7;
+// console.log(typeof name);
+
+
+//undefined
+let surname;
+console.log(typeof surname);
+
+
+//boolean
+let selected = false;
+console.log(typeof selected);
+
+
+//null
+let lastname = null
+console.log(typeof lastname);
+
+
+//number type
+let numb = 12;
+console.log(typeof numb);
+
+
